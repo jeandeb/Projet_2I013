c69c2606f5829d81b8e4decab6da9ce30fd23ee5
KARMIM YANNIS <3414093@ppti-14-303-05.ufr-info-p6.jussieu.fr>
2017-01-30 19:44:56 +0100

    Version du 30/01

--- tools.py
+++ tools.py
@@ -15,8 +15,8 @@ maxBallAcceleration = 5 # Acceleration maximale de la balle
 
 but2 = Vector2D( GAME_WIDTH, GAME_HEIGHT/2. )
 but1 = Vector2D( 0, GAME_HEIGHT/2 )
-class MyState(object):
 
+class properties(object):
     def __init__(self,state,idteam,idplayer):
         self.state=state
         self.key=(idteam,idplayer)
@@ -28,39 +28,194 @@ class MyState(object):
             self.owngoal =  Vector2D( GAME_WIDTH, GAME_HEIGHT/2. )
         self.my_position =  self.state.player_state( self.key[0], self.key[1] ).position
         self.ball_position = self.state.ball.position
-    
-    def go(self,p):
-        return SoccerAction(p-self.my_position,Vector2D())
-    
-    def shoot(self,p):
-        return SoccerAction(Vector2D(),p-self.my_position)
         
+    @property
     def vector_ball(self) :
         return  self.ball_position - self.my_position
+        
+    @property
+    def dist_ball(self) :
+        return  self.vector_ball.norm
+        
+    def ball_area(self, dist ) : 
+        return (self.ball_position - self.owngoal).norm < dist 
+          
     
-    def go_ball(self) : 
-        return SoccerAction(self.vector_ball(),Vector2D())
-        
-    def is_goal(self) : 
-        if (self.ball_position - self.owngoal).norm < 30 :
-            return True
-        return False
-        
+    @property
+    def ball_side(self):
+        return not self.ball_area( GAME_WIDTH/2 )
+ 
+    
+    @property
     def can_shoot(self) :
-        if ( self.vector_ball() ).norm >= PLAYER_RADIUS + BALL_RADIUS:
+        if ( self.vector_ball ).norm >= PLAYER_RADIUS + BALL_RADIUS:
                 return False
         return True
-    
-    def shoot_goal(self):
-        return SoccerAction(Vector2D(),self.adgoal-self.my_position)
-        
-        
+         
+    @property
     def ball_move(self):
-        return self.state.ball.vitesse.x > 0 or self.state.ball.vitesse.y > 0         
-        
-    def placement(self):
-        return self.go((self.ball_position + self.owngoal)/2 )
+        return self.state.ball.vitesse.x > 0 or self.state.ball.vitesse.y > 0  
     
+    @property
+    def team_players(self):
+        liste_tp = []
+        for idt,idp in self.state.players : 
+            if idt== self.key[0] and idp != self.key[1] :
+                dist = (  self.state.player_state( idt, idp ).position - self.my_position ).norm
+                liste_tp.append([idt,idp, dist])
+        return liste_tp
+    
+    @property
+    def adv_players(self):
+        liste_ap = []
+        for idt,idp in self.state.players : 
+            if idt.id_team != self.key[0] and idp.id_player != self.key[1] :
+                dist = ( idp.position.norm -  self.state.player_state( self.key[0], self.key[1] ).position ).norm
+                liste_ap.append([idt,idp, dist])
+        return liste_ap     
+    
+    
+    def dist_min(self,liste):
+        mini = GAME_WIDTH+GAME_HEIGHT
+        for i in liste:
+            if i[2]<mini:
+                
+                mini=i[2]
+                idmin = i[1]
+        return [i[0], idmin]
+    
+    @property
+    def pos_dist_min(self) :
+        dist_min =  self.dist_min( self.team_players )
+        return self.state.player_state( dist_min[0],  dist_min[1] ).position
+    
+    
+   
+    
+    
+
+class basic_action(object): 
+    
+    def __init__(self,properties):
+        self.prop = properties
+    
+    def passe(self,p):
+        return SoccerAction(Vector2D(),p-self.prop.my_position)
+    
+    def go(self,p):
+        return SoccerAction(p-self.prop.my_position,Vector2D())
+    
+    @property    
+    def go_ball(self) : 
+        return SoccerAction(self.prop.vector_ball,Vector2D())
+    
+    @property
+    def shoot_goal(self):
+        return SoccerAction(Vector2D(),self.prop.adgoal - self.prop.my_position)
+        
+    @property
+    def placement_def(self):
+        return self.go((self.prop.ball_position + self.prop.owngoal)/2 )
+        
+    @property
+    def placement_att(self):
+        if self.prop.ball_side :
+            return self.go( Vector2D( GAME_WIDTH + 3, GAME_HEIGHT ) )
+            
+    @property
+    def placement_att_sup(self):
+        nearplayer = self.prop.pos_dist_min
+        return self.go( nearplayer + Vector2D( 40, 0 ) )
+        
+    def conduire( self, direction ):
+        return
+        
+        
+def fonceur( basic_action ):
+    
+    if not basic_action.prop.can_shoot : 
+           return basic_action.go_ball
+           
+    return basic_action.shoot_goal
+    
+def passeur( basic_action ):
+    prop = basic_action.prop
+    if not prop.can_shoot : 
+           return basic_action.go_ball
+      
+    nearplayer = prop.pos_dist_min
+    return basic_action.passe( nearplayer )
+
+#def dribbleur( basic_action )       
+    
+def defence_off(basic_action):
+    
+    prop = basic_action.prop
+    if prop.ball_area(30) :
+        return fonceur(basic_action)
+                      
+    if not prop.ball_move:
+        return basic_action.placement_def
+
+         
+    if not prop.can_shoot : 
+        return basic_action.go_ball
+           
+    return basic_action.shoot_goal
+
+def conduite_but( basic_action ):
+
+    
+
+    return     
+    
+    
+
+def defence(basic_action):
+    
+    prop = basic_action.prop
+    if prop.ball_area(30) :
+        return passeur(basic_action)
+        
+    if prop.ball_side or not prop.ball_move:
+        return basic_action.placement_def
+         
+    if not prop.can_shoot : 
+        return basic_action.go_ball
+           
+    return basic_action.shoot_goal
+        
+def striker( basic_action ):
+    
+    prop = basic_action.prop
+    
+    if prop.ball_side or prop.dist_ball < 15  :
+        return fonceur(basic_action)
+        
+    return basic_action.placement_att_sup
+        
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
         
         

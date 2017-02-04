ad250483c45bc4c41310b439d673782188d6da68
Morgan Carmellini <morgancarmellini@MacBook-Air-de-Morgan.local>
2017-02-04 23:54:11 +0100

    Version prof

--- __init__.py
+++ __init__.py
@@ -1,10 +1,15 @@
+from soccersimulator.strategies  import Strategy
+from soccersimulator.mdpsoccer import SoccerTeam, Simulation, SoccerAction
+from soccersimulator.gui import SimuGUI,show_state,show_simu
+from soccersimulator.utils import Vector2D
+import tools
 import strategy
 
 ## Creation d'une equipe
 team1 = strategy.SoccerTeam( name = "team1" ,login = "etu1" )
 team2 = strategy.SoccerTeam( name = "team2", login = "etu2" )
 
-team1.add( "Johny", strategy.StrikerStrategy( ) ) 
+team1.add( "Johny", strategy.DefenceOffStrategy( ) ) 
 #team1.add( "Pierre", strategy.DefenceStrategy( ) )
-#team2.add( "Yannis", strategy.DefenceStrategy( ) )
-team2.add( "Larti", strategy.FonceurStrategy( ) )    
+team2.add( "Yannis", strategy.DefenceStrategy( ) )
+team2.add( "Larti", strategy.StrikerStrategy( ) )    

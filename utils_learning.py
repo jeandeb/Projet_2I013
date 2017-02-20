4b0a0e84b826c82316fdf8bb85258a45e753d7af
KARMIM YANNIS <3414093@ppti-14-303-17.ufr-info-p6.jussieu.fr>
2017-02-20 19:37:05 +0100

    20/02

---
+++ utils_learning.py
@@ -0,0 +1,38 @@
+from soccersimulator.utils import Vector2D
+from soccersimulator.mdpsoccer import Ball
+import strategy
+
+class Observer( object ):
+    MAX_STEP = 20
+    
+    def __init__(self, simu):
+        self.simu = simu
+        self.simu.listeners += self
+        
+    def begin_match( self, team1, team2, state ):
+        self.simu.state.states[(1,0)].position=Vector2D(130,45)
+        self.simu.state.states[(2,0)].position=Vector2D(0,0)
+        self.simu.state.ball = Ball( Vector2D(130,45) ) 
+        self.last, self.cpt, self.cpt_tot = 0, 0, 0
+        
+    def begin_round( self, team1, team2, state ):
+        self.simu.state.states[(1,0)].position=Vector2D(130,45)
+        self.simu.state.states[(2,0)].position=Vector2D(0,0)
+        self.simu.state.ball = Ball( Vector2D(130,45) ) 
+        
+    def update_round(self,team1,team2,state):
+        if state.step > self.last + self.MAX_STEP : 
+            self.simu.end_round()
+            
+    def end_round(self,team1,team2,state):
+        if state.goal > 0 :
+            self.cpt += 1
+        self.cpt_tot += 1
+        if self.cpt_tot > 1000 : 
+            self.simu.end_match()
+    
+    
+            
+        
+        
+    

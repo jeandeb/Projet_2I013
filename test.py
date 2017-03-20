eff95792737bdf6553dab7d2d1bc7c973073afee
KARMIM YANNIS <3414093@ppti-14-303-20.ufr-info-p6.jussieu.fr>
2017-03-20 18:46:47 +0100

    oooo

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-<
 
 import strategy
-
+import apprentissage_arbres
+from arbres_utils import build_apprentissage,affiche_arbre,DTreeStrategy,apprend_arbre,genere_dot
 ## Creation d'une equipe
 team1 = strategy.SoccerTeam( name = "AS FORBACH" ,login = "etu1" )
 team2 = strategy.SoccerTeam( name = "MFC", login = "etu2" )
@@ -15,6 +16,11 @@ team1.add( "Cavani", strategy.FonceurStrategy( ) )
 #team2.add( "Fillon", strategy.DefenceOffStrategy( ) )
 #team2.add( "IEUoeio", strategy.StrikerStrategy( ) )
 team2.add( "Aurier", strategy.SoloStrategy( ) )
+dic = {"Fonceur":strategy.FonceurStrategy(),"DefPlacement":strategy.DefPlacement(),"Static":strategy.StaticStrategy()}
+treeStrat1 = DTreeStrategy(dt,dic,my_get_features)
+team2.add("Benzema",treeStrat1)
+
+
 #Creation d'une partie
 simu = strategy.Simulation( team1, team2 )
 #Jouer et afficher la partie
